fae50ccf57aa097854491c9f25dd795627676e06
Hossam <hossamnasser938@gmail.com>
2021-04-27 04:28:58 +0200

    fix weekly calendar rotation issue

--- src/expandableCalendar/weekCalendar.js
+++ src/expandableCalendar/weekCalendar.js
@@ -56,7 +56,7 @@ class WeekCalendar extends Component {
   componentDidUpdate(prevProps) {
     const {updateSource, date} = this.props.context;
 
-    if (date !== prevProps.context.date && updateSource !== UPDATE_SOURCES.WEEK_SCROLL) {
+    if (date !== prevProps.context.date && updateSource !== UPDATE_SOURCES.WEEK_SCROLL || prevProps.calendarWidth !== this.props.calendarWidth) {
       this.setState({items: this.getDatesArray()});
       this.list.current.scrollToIndex({animated: false, index: NUMBER_OF_PAGES});
     }
